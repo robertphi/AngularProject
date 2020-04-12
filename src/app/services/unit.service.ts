1085a45589cccebce99c48db26e91f0db959a5e6
robertphi <robertphi@gmail.com>
2020-04-11 22:10:48 -0500

    #1 Create Unit Page clean-up

--- src/app/services/unit.service.ts
+++ src/app/services/unit.service.ts
@@ -1,25 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import {DefaultDataService, EntityCollectionServiceBase, EntityCollectionServiceElementsFactory, HttpUrlGenerator} from '@ngrx/data';
-// import {Unit} from '../models/Unit';
-// import {HttpClient} from '@angular/common/http';
-//
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class UnitService extends DefaultDataService<Unit>{
-//
-//   constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator ) {
-//     super('Unit', http, httpUrlGenerator);
-//     // super('Unit', serviceElementsFactory);
-//   }
-//
-//
-//
-//
-//
-// }
-
-
 import {Injectable} from '@angular/core';
 import {
   EntityCollectionServiceBase,
